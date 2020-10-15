ee05884905b0a6de85d71e967e276fa0213ad8a1
Stefan Boos <stefan.boos@gmx.de>
2020-10-15 22:13:10 +0200

    feat: group hello_note into misc items

--- src/main/java/systems/boos/sboosmodhello/ModItems.java
+++ src/main/java/systems/boos/sboosmodhello/ModItems.java
@@ -1,11 +1,12 @@
 package systems.boos.sboosmodhello;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "sboosmodhello");
-    public static final RegistryObject<Item> HELLO_NOTE = ITEMS.register("hello_note", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> HELLO_NOTE = ITEMS.register("hello_note", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
 }
