06abbe87f3b9b776feec18e639f311b954f8f217
Stefan Boos <stefan.boos@gmx.de>
2020-10-19 22:20:47 +0200

    build: add stick with damage

--- src/main/java/systems/boos/sboosmodhello/ModItems.java
+++ src/main/java/systems/boos/sboosmodhello/ModItems.java
@@ -1,8 +1,6 @@
 package systems.boos.sboosmodhello;
 
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -10,5 +8,6 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "sboosmodhello");
     public static final RegistryObject<Item> HELLO_NOTE = ITEMS.register("hello_note", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+    public static final RegistryObject<Item> HELLO_STICK = ITEMS.register("hello_stick", () -> new SwordItem(ItemTier.DIAMOND, 10, 2.0f, new Item.Properties().group(ItemGroup.COMBAT)));
     public static final RegistryObject<Item> HELLO_BLOCK = ITEMS.register("hello_block", () -> new BlockItem(ModBlocks.HELLO_BLOCK.get(), new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)));
 }
